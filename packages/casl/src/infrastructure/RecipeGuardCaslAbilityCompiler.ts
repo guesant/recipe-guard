3952a5fffb27b549c555617a86bcc4a3785920cb
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2023-09-17 18:02:46 -0400

    feat(packages/casl): compiler: validate constraint recipe

--- packages/casl/src/infrastructure/RecipeGuardCaslAbilityCompiler.ts
+++ packages/casl/src/infrastructure/RecipeGuardCaslAbilityCompiler.ts
@@ -1,6 +1,7 @@
 import {
   AuthorizationConstraintRecipeResolutionMode,
   AuthorizationConstraintRecipeType,
+  AuthorizationConstraintRecipeZod,
   IAuthorizationConstraintRecipe,
   IRecipeGuardContext,
 } from "#recipe-guard-core";
@@ -17,73 +18,83 @@ export class RecipeGuardCaslAbilityCompiler {
     abilityBuilder: AbilityBuilder<AnyAbility>,
     verbo: string,
     recurso: string,
-    authorizationConstraintRecipe: IAuthorizationConstraintRecipe,
+    authorizationConstraintRecipeRaw: IAuthorizationConstraintRecipe,
     targetEntityId: unknown | null = null
   ) {
     const { can: allow, cannot: forbid } = abilityBuilder;
 
-    switch (authorizationConstraintRecipe.resolutionMode) {
-      case AuthorizationConstraintRecipeResolutionMode.CASL_ONLY: {
-        switch (authorizationConstraintRecipe.type) {
-          case AuthorizationConstraintRecipeType.BOOLEAN: {
-            if (authorizationConstraintRecipe.value) {
-              allow(verbo, recurso);
-            } else {
-              forbid(verbo, recurso);
+    const validationResult = AuthorizationConstraintRecipeZod.safeParse(
+      authorizationConstraintRecipeRaw
+    );
+
+    if (validationResult.success) {
+      const authorizationConstraintRecipe = <IAuthorizationConstraintRecipe>(
+        validationResult.data
+      );
+
+      switch (authorizationConstraintRecipe.resolutionMode) {
+        case AuthorizationConstraintRecipeResolutionMode.CASL_ONLY: {
+          switch (authorizationConstraintRecipe.type) {
+            case AuthorizationConstraintRecipeType.BOOLEAN: {
+              if (authorizationConstraintRecipe.value) {
+                allow(verbo, recurso);
+              } else {
+                forbid(verbo, recurso);
+              }
+
+              break;
             }
 
-            break;
-          }
+            case AuthorizationConstraintRecipeType.FILTER: {
+              const forbidMode =
+                authorizationConstraintRecipe.resolutionModeCaslForbid;
 
-          case AuthorizationConstraintRecipeType.FILTER: {
-            const forbidMode =
-              authorizationConstraintRecipe.resolutionModeCaslForbid;
+              if (forbidMode) {
+                forbid(verbo, recurso, authorizationConstraintRecipe.condition);
+              } else {
+                allow(verbo, recurso, authorizationConstraintRecipe.condition);
+              }
 
-            if (forbidMode) {
-              forbid(verbo, recurso, authorizationConstraintRecipe.condition);
-            } else {
-              allow(verbo, recurso, authorizationConstraintRecipe.condition);
+              break;
             }
-
-            break;
           }
-        }
 
-        break;
-      }
-
-      case AuthorizationConstraintRecipeResolutionMode.RESOLVE_AND_MERGE:
-      case AuthorizationConstraintRecipeResolutionMode.RESOLVE_AND_EXCLUDE: {
-        const appResource = await this.authorizationContext.getAppResource(
-          recurso
-        );
-
-        if (!appResource) {
           break;
         }
 
-        const resolvedIds =
-          await this.recipeGuardCaslAbilityCompilerContext.getResolvedIdsByAppResourceAuthorizationConstraintRecipe(
-            appResource,
-            authorizationConstraintRecipe,
-            targetEntityId
+        case AuthorizationConstraintRecipeResolutionMode.RESOLVE_AND_MERGE:
+        case AuthorizationConstraintRecipeResolutionMode.RESOLVE_AND_EXCLUDE: {
+          const appResource = await this.authorizationContext.getAppResource(
+            recurso
           );
 
-        const condition = {
-          id: {
-            $in: [...resolvedIds],
-          },
-        };
-
-        switch (authorizationConstraintRecipe.resolutionMode) {
-          case AuthorizationConstraintRecipeResolutionMode.RESOLVE_AND_MERGE: {
-            allow(verbo, recurso, condition);
+          if (!appResource) {
             break;
           }
 
-          case AuthorizationConstraintRecipeResolutionMode.RESOLVE_AND_EXCLUDE: {
-            forbid(verbo, recurso, condition);
-            break;
+          const resolvedIds =
+            await this.recipeGuardCaslAbilityCompilerContext.getResolvedIdsByAppResourceAuthorizationConstraintRecipe(
+              appResource,
+              authorizationConstraintRecipe,
+              targetEntityId
+            );
+
+          const condition = {
+            id: {
+              $in: [...resolvedIds],
+            },
+          };
+
+          switch (authorizationConstraintRecipe.resolutionMode) {
+            case AuthorizationConstraintRecipeResolutionMode.RESOLVE_AND_MERGE: {
+              allow(verbo, recurso, condition);
+              break;
+            }
+
+            case AuthorizationConstraintRecipeResolutionMode.RESOLVE_AND_EXCLUDE: {
+              forbid(verbo, recurso, condition);
+              break;
+            }
           }
         }
       }
